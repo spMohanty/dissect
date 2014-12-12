f9bf09cc97d6057afdf429c8d0d038b559338fd3
S.P. Mohanty <spmohanty91@gmail.com>
2014-12-12 12:23:06 +0530

    adding example of Weight Additive Training

--- src/examples/word2vec_example.py
+++ src/examples/word2vec_example.py
@@ -18,6 +18,8 @@ import composes.utils.scoring_utils as scoring_utils
 print "Loading the data..."
 data_path = "/Users/spmohanty/work/compositional_semantics/dissect/test_data/"
 
+dissect_toy_data = "/Users/spmohanty/work/compositional_semantics/dissect/src/examples/data/"
+
 
 ## Only adding support for .dm files, as the sm files are inherently inefficiently designed 
 ## especially for latge vector spaces. :( 
@@ -50,8 +52,6 @@ print space.get_neighbours("cat", 3, CosSimilarity())
 # print space.id2row
 
 
-
-
 # instantiate a weighted additive model
 my_comp = WeightedAdditive(alpha = 1, beta = 1)
 
@@ -63,6 +63,41 @@ composed_space = my_comp.compose([("good", "book", "good_book"),
 print composed_space.id2row
 print composed_space.cooccurrence_matrix
 print composed_space.get_sims([("good_car","good_book")], CosSimilarity()) # Similarity metric
+
+#===============================================================================================================
+print "="*80
+#===============================================================================================================
+##Training Models
+
+##Training Weight Additive Models
+
+#training data
+train_data = [("good", "car", "good_car"),
+              ("good", "book", "good_book")
+              ]
+
+## Demonstrating on a smaller vector space for performance and demo
+##	But the word2vec space can also be used, as it is just another object of the same class
+#load an argument space
+arg_space = io_utils.load(dissect_toy_data+"out/ex10.pkl")
+print arg_space.id2row
+print arg_space.cooccurrence_matrix
+
+#load a phrase space
+phrase_space = io_utils.load(dissect_toy_data+"out/PHRASE_SS.ex10.pkl")
+print phrase_space.id2row
+print phrase_space.cooccurrence_matrix
+
+#train a weighted additive model on the data
+my_comp = WeightedAdditive()
+my_comp.train(train_data, arg_space, phrase_space)
+
+#print its parameters
+print "Weight Additive Model :: "
+print "alpha:", my_comp.alpha
+print "beta:", my_comp.beta
+
+
 
 
 
