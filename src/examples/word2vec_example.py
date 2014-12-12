aff95799aed9725eca10fbc794458cf6fa4ed4e2
S.P. Mohanty <spmohanty91@gmail.com>
2014-12-12 12:24:58 +0530

    Adding example of training of Time Dilation Model

--- src/examples/word2vec_example.py
+++ src/examples/word2vec_example.py
@@ -97,7 +97,38 @@ print "Weight Additive Model :: "
 print "alpha:", my_comp.alpha
 print "beta:", my_comp.beta
 
+#===============================================================================================================
+print "="*80
+print "Time Dilation model"
+#===============================================================================================================
 
+#training data
+train_data = [("good", "car", "good_car"),
+              ("good", "book", "good_book")
+              ]
+
+#load an argument space
+arg_space = io_utils.load(dissect_toy_data+"out/ex10.pkl")
+
+#load a phrase space
+phrase_space = io_utils.load(dissect_toy_data+"out/PHRASE_SS.ex10.pkl")
+print "Training phrase space"
+print phrase_space.id2row
+print phrase_space.cooccurrence_matrix
+
+#train a Dilation model on the data
+my_comp = Dilation()
+my_comp.train(train_data, arg_space, phrase_space)
+
+#print its parameters
+print "\nlambda:", my_comp._lambda
+
+#use the model to compose the train data
+composed_space  = my_comp.compose([("good", "bike", "good_bike")], 
+                                  arg_space)
+print "\nComposed space:" 
+print composed_space.id2row
+print composed_space.cooccurrence_matrix
 
 
 
