495ac61003fea6d2b3b67ea88dc745abdc9b4c55
S.P. Mohanty <spmohanty91@gmail.com>
2014-12-12 12:28:59 +0530

    Adding example of training of Full Additive model

--- src/examples/word2vec_example.py
+++ src/examples/word2vec_example.py
@@ -129,6 +129,39 @@ composed_space  = my_comp.compose([("good", "bike", "good_bike")],
 print "\nComposed space:" 
 print composed_space.id2row
 print composed_space.cooccurrence_matrix
+#===============================================================================================================
+print "="*80
+print "Full Additive model"
+#===============================================================================================================
+
+#training data
+train_data = [("good", "car", "good_car"),
+              ("good", "book", "good_book")
+              ]
+
+#load an argument space
+arg_space = io_utils.load(dissect_toy_data+"out/ex10.pkl")
+
+#load a phrase space
+phrase_space = io_utils.load(dissect_toy_data+"out/PHRASE_SS.ex10.pkl")
+print "Training phrase space"
+print phrase_space.id2row
+print phrase_space.cooccurrence_matrix
+
+#train a FullAdditive model on the data
+my_comp = FullAdditive()
+my_comp.train(train_data, arg_space, phrase_space)
+
+#print its parameters
+print "\nA:", my_comp._mat_a_t.transpose()
+print "B:", my_comp._mat_b_t.transpose()
+
+#use the model to compose the train data
+composed_space  = my_comp.compose([("good", "bike", "good_bike")], 
+                                  arg_space)
+print "\nComposed space:" 
+print composed_space.id2row
+print composed_space.cooccurrence_matrix
 
 
 
